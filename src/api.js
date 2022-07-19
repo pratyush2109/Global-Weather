d9067e457455c93153676a2c173f2bfe5e0ef2bd
pratyush2109 <pratyush.ambastha21@gmail.com>
2022-07-19 22:18:11 +0530

    Completed weather-app

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '473196f843mshcd31108fd6b4575p1cd680jsnc55f33c65c9c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "86ec6189bc81f3414463b78341c151e2";
